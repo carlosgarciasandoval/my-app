9e72892b6916a3d7a7b708d853c626e4ba26df6e
carlosgarciasandoval <carlosgarciasandoval85@gmail.com>
2022-01-09 17:51:59 +0100

    Nombre del proyecto: my/app, descripcion del proyecto: version 1.0 inicial

---
+++ src/main/java/com/carlos/myapp/entity/Test.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.carlos.myapp.entity;
+
+/**
+ * @author msi
+ *
+ */
+public class Test {
+	
+	// Propiedad de prueba para compilar en archivo TESTENTITY
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
